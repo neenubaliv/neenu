0409a69c03e472fd44f26ed115f9fdca4f9886f6
NeenuBal <neenu@stayntouch.com>
2016-12-30 17:58:18 +0530

    Initial commit

---
+++ home.js
@@ -0,0 +1,47 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/login', {
+        templateUrl: 'pages/login.html',
+        controller: 'LoginController',
+        activetab: 'login'
+    })
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'HomeController',
+    activetab: 'home'
+  })
+
+  .when('/members', {
+    templateUrl : 'pages/members.html',
+    controller  : 'MembersController'
+  })
+
+
+  .otherwise({redirectTo: '/'});
+});
+	
+app.controller('HomeController', function($scope) {
+  $scope.message = 'Website ';
+   $scope.stepsModel = [];
+
+    $scope.imageUpload = function(element){
+        var reader = new FileReader();
+        reader.onload = $scope.imageIsLoaded;
+        reader.readAsDataURL(element.files[0]);
+    }
+
+    $scope.imageIsLoaded = function(e){
+        $scope.$apply(function() {
+            $scope.stepsModel.push(e.target.result);
+        });
+    }
+    function widgetsController($scope, $route) {
+    $scope.$route = $route;
+}
+});
+
+app.controller('MembersController', function($scope) {
+  $scope.message = 'Members ';
+});
